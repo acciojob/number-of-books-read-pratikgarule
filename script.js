34233d603f3b49967ab6577c3c3c767a95c0ec5c
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-04-15 15:31:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,16 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	for (var i = 0; i < library.length; i++) 
+   {
+    var book = "'" + library[i].title + "'" + ' by ' + library[i].author + ".";
+    if (library[i].readingStatus) {
+      console.log("Already read " + book);
+    } else
+    {
+     console.log("You still need to read " + book);
+    }
+   }
 };
 
 // Do not change the code below
